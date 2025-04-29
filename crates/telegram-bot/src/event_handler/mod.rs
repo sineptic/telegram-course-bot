0e8febcd02ae00ed6c34c8515382938f2cd225f1
sineptic <sineptic0@gmail.com>
2025-04-29 20:25:22 +0300

    extract variables to BotCtx

--- crates/telegram-bot/src/event_handler/mod.rs
+++ crates/telegram-bot/src/event_handler/mod.rs
@@ -1,7 +1,4 @@
-use std::{collections::HashMap, str::FromStr};
-
-use course_graph::graph::CourseGraph;
-use rand::SeedableRng;
+use ctx::BotCtx;
 use teloxide::{Bot, prelude::Requester, types::UserId};
 use tokio::sync::oneshot;
 
@@ -12,25 +9,70 @@ use crate::{
     utils::ResultExt,
 };
 
-pub(crate) async fn event_handler(bot: Bot, mut rx: EventReceiver) {
-    let course_graph = CourseGraph::from_str(&std::fs::read_to_string("graph").unwrap())
-        .unwrap_or_else(|err| {
-            println!("{err}");
-            panic!("graph parsing error");
-        });
-    let mut progress_store = HashMap::new();
-    course_graph.init_store(&mut progress_store);
-    let base_graph = course_graph.generate_graph();
+pub(crate) mod ctx {
+    use std::{
+        collections::{BTreeMap, HashMap},
+        str::FromStr,
+    };
 
-    let deque = deque::from_str(&std::fs::read_to_string("cards.md").unwrap(), true).unwrap();
-    let mut rng = rand::rngs::StdRng::from_os_rng();
+    use course_graph::{graph::CourseGraph, progress_store::TaskProgress};
+    use graphviz_rust::dot_structures::Graph;
+    use rand::{SeedableRng, rngs::StdRng};
+    use teloxide::Bot;
+
+    use crate::interaction_types::{Task, deque};
+
+    pub struct BotCtx {
+        course_graph: CourseGraph,
+        pub progress_store: HashMap<String, TaskProgress>,
+        base_graph: Graph,
+        pub deque: BTreeMap<String, BTreeMap<u16, Task>>,
+        pub rng: StdRng,
+        bot: Bot,
+    }
+
+    impl BotCtx {
+        pub fn load(bot: Bot) -> Self {
+            let course_graph = CourseGraph::from_str(&std::fs::read_to_string("graph").unwrap())
+                .unwrap_or_else(|err| {
+                    println!("{err}");
+                    panic!("graph parsing error");
+                });
+            let mut progress_store = HashMap::new();
+            course_graph.init_store(&mut progress_store);
+            let base_graph = course_graph.generate_graph();
+
+            let deque =
+                deque::from_str(&std::fs::read_to_string("cards.md").unwrap(), true).unwrap();
+            let rng = StdRng::from_os_rng();
+
+            Self {
+                course_graph,
+                progress_store,
+                base_graph,
+                deque,
+                rng,
+                bot,
+            }
+        }
+        pub fn base_graph(&self) -> Graph {
+            self.base_graph.clone()
+        }
+        pub fn bot(&self) -> Bot {
+            self.bot.clone()
+        }
+    }
+}
+
+pub(crate) async fn event_handler(bot: Bot, mut rx: EventReceiver) {
+    let mut ctx = BotCtx::load(bot);
 
     while let Some(event) = rx.recv().await {
         match event {
             Event::ReviseCard { user_id, card_name } => {
-                let Some(tasks) = deque.get(&card_name.to_lowercase()) else {
+                let Some(tasks) = ctx.deque.get(&card_name.to_lowercase()) else {
                     send_interactions(
-                        bot.clone(),
+                        ctx.bot(),
                         user_id,
                         vec![TelegramInteraction::Text(
                             "Card with this name not found".into(),
@@ -40,25 +82,25 @@ pub(crate) async fn event_handler(bot: Bot, mut rx: EventReceiver) {
                     .log_err();
                     continue;
                 };
-                let task = card::random_task(tasks, &mut rng).clone();
+                let task = card::random_task(tasks, &mut ctx.rng).clone();
                 let (tx, rx) = oneshot::channel();
                 tokio::spawn(event_end_handler(
-                    bot.clone(),
+                    ctx.bot(),
                     rx,
                     user_id,
                     task.correct_answer().to_owned(),
                     task.explanation.clone(),
                 ));
-                set_task_for_user(bot.clone(), user_id, task.interactions(), tx)
+                set_task_for_user(ctx.bot(), user_id, task.interactions(), tx)
                     .await
                     .log_err();
             }
 
             Event::ViewGraph { user_id } => {
                 let graph_image =
-                    course_graph::generate_graph_chart(base_graph.clone(), &progress_store);
+                    course_graph::generate_graph_chart(ctx.base_graph(), &ctx.progress_store);
                 send_interactions(
-                    bot.clone(),
+                    ctx.bot(),
                     user_id,
                     vec![TelegramInteraction::PersonalImage(graph_image)],
                 )
